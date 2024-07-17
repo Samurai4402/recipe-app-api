7d4e63791d10bae157143fb411c424af4b623226
Samurai4402 <dinmukhamed.shabarov@gmail.com>
2024-07-17 15:21:28 +0500

    Completed Recipe API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -10,5 +10,13 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for a recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+        
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,8 +5,9 @@ from django.contrib.auth import (
         get_user_model,
         authenticate
     )
-from rest_framework import serializers
 from django.utils.translation import gettext as _
+
+from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
