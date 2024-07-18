1192dc86d15822928f47a94c3f7a0995fd867152
Samurai4402 <dinmukhamed.shabarov@gmail.com>
2024-07-18 13:30:35 +0500

    Nested Serializer

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,10 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelsTests(TestCase):
     """Tests models"""
@@ -65,5 +69,13 @@ class ModelsTests(TestCase):
             price=Decimal('5.50'),
             description='Sample recipe description',
         )
-        
+
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+
+        self.assertEqual(str(tag), tag.name)
